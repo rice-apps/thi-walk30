81464f45a52fafd50b5f70c0208da2dd812958e6
Evan-Stegall <evanstegall007@gmail.com>
2023-10-21 13:49:24 -0500

    added create route

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Event = require('../models/events');
 
 router.post("/create", async (req, res, next) {
+    const event = new Event({
+        id: req.body.id,
+        title: req.body.title,
+        description: req.body.description,
+        featured_img: req.body.featured_img,
+        link: req.body.link
+    })
+
+    try {
+        const savedData = await event.save();
+        res.status(200).json(savedData);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 
 });
 router.delete("/delete", async (req, res, next) {
