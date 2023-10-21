9572de2f4afa6b0e58338a75b74e9e03b9d0f2d6
trietmchau <trietmchau@gmail.com>
2023-10-21 13:55:23 -0500

    Add event routes

--- server/routes/events.js
+++ server/routes/events.js
@@ -3,8 +3,23 @@ const Event = require("../models/events");
 const router = express.Router();
 
 router.post("/create", async (req, res, next) {
+    const event = new Event({
+        id: req.body.id,
+        title: req.body.title,
+        description: req.body.description,
+        featured_img: req.body.featured_img,
+        link: req.body.link
+    })
+
+    try {
+        const savedData = await event.save();
+        res.status(200).json(savedData);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 
 });
+
 router.delete("/delete-event/:id", async (req, res, next) {
     try {
         const id = req.params.id
