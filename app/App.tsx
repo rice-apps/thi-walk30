917673d67ec8863bcc7c593ef2f25a7b98c430cb
MacAjwani <mayankajwani@gmail.com>
2023-10-28 15:34:37 -0500

    Started implementing phone number input

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Example from './components/sign-in';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Example></Example>
       <StatusBar style="auto" />
     </View>
   );
