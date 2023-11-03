d9268a12d237ca1e2020f20ff648e7888430b9da
nolantuite <nrt5@rice.edu>
2023-11-02 20:46:54 -0500

    fixed server issue

--- server/index.js
+++ server/index.js
@@ -9,23 +9,9 @@ const { default: mongoose } = require('mongoose');
 
 const uri = "mongodb+srv://" + process.env.MONGO_ADMIN_USERNAME + ":" + process.env.MONGO_ADMIN_PASSWORD + "@thi-cluster.hppzt17.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-
 async function run() {  
     try {
-        // Connect the client to the server
-        // await client.connect();
-        // // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-
-        await mongoose.connect(uri);
+        await mongoose.connect(uri)
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
         const app = express();
 
@@ -37,9 +23,8 @@ async function run() {
         app.listen(3000, () => {
             console.log(`Server started at ${3000}`);
         })
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await mongoose.connection.close();
+    } catch (error) {
+        console.log(error);
     }
 }
 
