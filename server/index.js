dc587e98c23e640c6396a513ba90bf4d94ce63a5
nolantuite <nrt5@rice.edu>
2023-11-02 20:24:17 -0500

    init

--- server/index.js
+++ server/index.js
@@ -4,25 +4,28 @@ const express = require('express');
 const routes = require("./routes");
 const cors = require('cors');
 const morgan = require("morgan");
+const { default: mongoose } = require('mongoose');
 
 
 const uri = "mongodb+srv://" + process.env.MONGO_ADMIN_USERNAME + ":" + process.env.MONGO_ADMIN_PASSWORD + "@thi-cluster.hppzt17.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
 
 async function run() {  
     try {
         // Connect the client to the server
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.connect();
+        // // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+        await mongoose.connect(uri);
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
         const app = express();
 
@@ -36,7 +39,7 @@ async function run() {
         })
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        await mongoose.connection.close();
     }
 }
 
