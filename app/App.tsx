66001601421f25b4f634110847a18d97d7c1dd70
justin <jhp9@rice.edu>
2023-11-04 15:09:41 -0500

    Formatted the events page

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,14 @@
+import {EventPage} from './eventpage/EventPage';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <EventPage title = {"Walk to Defeat ALS Houston"} body = {"Walk with us to help fundraise for ALS patients"} date = {"10/28/2023"} hostorg = {"Texas Heart Institue"} link = {'https://www.techonthenet.com/index.php'} />
     </View>
   );
 }
@@ -18,3 +21,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
