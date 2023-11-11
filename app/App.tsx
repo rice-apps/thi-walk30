bb04e13c18567bd70e7bb310544ac9a29c3fb4f2
justin <jhp9@rice.edu>
2023-11-11 13:56:17 -0600

    Moving files into type and reverting app file

--- app/App.tsx
+++ app/App.tsx
@@ -1,21 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {AnnouncementPage} from './announcementspage/AnnouncementPage';
 
 export default function App() {
-  interface LinkData {
-    title: string,
-    url: string
-  }
-  const linkArray: LinkData[] = [
-    { title: 'Example 1', url: 'https://www.example1.com' },
-    { title: 'Example 2', url: 'https://www.example2.com' },
-  ];
   
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <AnnouncementPage title = {"Announcements"} body = {"Walk with us to help fundraise for ALS patients"} linkMap = {linkArray}/>
       <StatusBar style="auto" />
     </View>
   );
