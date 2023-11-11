8afa3b30aa5a2d9ed7356185e117cc63edcbb652
Nolan Tuite <48373732+nolantuite@users.noreply.github.com>
2023-11-11 14:41:57 -0600

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
