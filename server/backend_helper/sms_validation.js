60f80f6603e83b8bb58764921d55e6aa8ded42f2
trietmchau <trietmchau@gmail.com>
2023-11-11 17:00:30 -0600

    Add valid SMS check

--- server/backend_helper/sms_validation.js
+++ server/backend_helper/sms_validation.js
@@ -5,18 +5,18 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
-const sendSms = (number, verifySid) => {
-  client.verify.v2
+const sendSms = async (number, verifySid) => {
+  return client.verify.v2
   .services(verifySid)
   .verifications.create({ to: number, channel: "sms" })
-  .then((verification) => verification.status);
+  .then((verification) => verification);
 }
 
-const verify = (number, verifySid, code) => {
-  client.verify.v2.services(verifySid)
+const verify = async (number, verifySid, code) => {
+  return client.verify.v2.services(verifySid)
       .verificationChecks
       .create({to: number, code: code})
-      .then(verification_check => verification_check.status);
+      .then(verification_check => verification_check);
 }
 
 module.exports = {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,8 +8,7 @@ const serviceSid = process.env.TWILIO_SERVICE_SID;
 router.get("/sendSms", async (req, res, next) => {
     try {
         const {phoneNumber} = req.body;
-        console.log(serviceSid);
-        const data = await smsValidation.sendSms(phoneNumber, serviceSid);
+        const data = await smsValidation.sendSms(phoneNumber, serviceSid)
         res.send(data);
     } catch (error) {
         res.status(400).json({message:error.message})
@@ -20,6 +19,9 @@ router.get("/verify", async (req, res, next) => {
     try {
         const {phoneNumber, code} = req.body;
         const data = await smsValidation.verify(phoneNumber, serviceSid, code);
+        if (data.valid == false) throw new Error("Wrong SMS Code")
+        
+        // Valid SMS, do something...
         res.send(data);
     } catch (error) {
         res.status(400).json({message:error.message})
