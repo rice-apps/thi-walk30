7e08e6bf26ae09e7ef627fbef48c73f670998706
Chase Hartsell <cmhartsell@me.com>
2024-01-20 15:05:06 -0600

    Add custom text component with Inter font

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Text from "./components/basic/Text";
 
 export default function App() {
   return (
---
+++ app/components/basic/Text.tsx
@@ -0,0 +1,36 @@
+import { Text as RNText, StyleProp, StyleSheet, TextStyle } from "react-native";
+import React from "react";
+import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from "@expo-google-fonts/inter";
+
+interface TextProps {
+    style?: StyleProp<TextStyle>,
+    weight?: number
+}
+
+export default function Text(props: React.PropsWithChildren<TextProps>) {
+    const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_500Medium, Inter_700Bold });
+
+    // Choose the correct font weight
+    let fontFamily;
+    switch (props.weight) {
+        case 400:
+            fontFamily = "Inter_400Regular";
+            break;
+        case 500:
+            fontFamily = "Inter_500Medium";
+            break;
+        case 700:
+            fontFamily = "Inter_700Bold";
+            break;
+        default:
+            fontFamily = "Inter_500Medium";
+    }
+
+    const style = StyleSheet.create({
+        text: {
+            fontFamily
+        }
+    });
+
+    return fontsLoaded ? <RNText style={[style.text, props.style]}>{props.children}</RNText> : <Text>{props.children}</Text>;
+}
