7691493720fbe1b121a28b606d4e70a12925aab0
justin <jhp9@rice.edu>
2024-01-20 14:21:20 -0600

    Changes to fit the design page

--- app/pages/AnnouncementPage.tsx
+++ app/pages/AnnouncementPage.tsx
@@ -1,47 +1,51 @@
 import React, { useState } from 'react';
-import { ScrollView, View, Text, StyleSheet, Button, TouchableHighlight, Linking, Image, FlatList } from 'react-native';  // <-- Import TouchableHighlight
+import { ScrollView, View, Text, StyleSheet, Button, TouchableHighlight, Linking, Image, FlatList, SafeAreaView } from 'react-native';  // <-- Import TouchableHighlight
 
 interface LinkData {
   title: string,
   url: string
 }
-export function AnnouncementPage(props: { title: string; body: string; linkMap: LinkData[]}) {
+export function AnnouncementPage(props: { title: string; body: string; hostingOrg: string; linkMap: LinkData[]}) {
 
   const onLinkPress = (link:string) => {
     Linking.openURL(link);
   };
 
   return (
-    <ScrollView style={{ margin: 10, flexDirection: 'column' }}>
-      
-      <Image
-        style = {{marginTop: 30, width: 400, height:300}}
-        source={{
-          uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }}
-      />
-      
-      <Text style = {{textAlign: 'center', fontSize: 30, marginBottom:50}}>{props.title}</Text> 
-      <View>
-        <Text style = {{fontFamily: 'Arial', fontSize: 18, marginBottom: 25}}>{props.body}</Text>
-      </View>
-  
-      <Text style = {{marginTop: 15, fontSize:20}}>Relevant Links</Text>
-      
-        {props.linkMap.map((links) => (
-          <TouchableHighlight onPress={() => onLinkPress(links.url)}>
-          <View>
-            <Text style ={{textDecorationLine: 'underline', color: 'blue', marginBottom: 10, marginTop: 10}}>{links.title}</Text>
-          </View>
-        </TouchableHighlight> 
-        ))}
-      
-      
-      {/* <TouchableHighlight onPress={onLinkPress}>
+    <SafeAreaView>
+      <ScrollView style={{flexDirection: 'column' }}>
+        
+        <Image
+          style = {{width: '100%', height:300}}
+          source={{
+            uri: 'https://www.texasheart.org/wp-content/uploads/2023/11/IMG_8671.jpeg',
+          }}
+        />
+
+        <Text style= {{color: 'rgb(16, 49, 88)', fontSize: 35, fontWeight:'600', paddingVertical:10, marginLeft:10}}>{props.hostingOrg}</Text>
+        
+        <Text style = {{color: 'rgb(16, 49, 88)', fontSize: 30, marginLeft:10, marginBottom:50, fontWeight:'500'}}>{props.title}</Text> 
         <View>
-          <Text style ={{textDecorationLine: 'underline', color: 'blue', marginBottom: 10, marginTop: 10}}>TechOnTheNet</Text>
+          <Text style = {{fontFamily: 'Arial', fontSize: 18, marginBottom: 25, marginLeft:10}}>{props.body}</Text>
         </View>
-      </TouchableHighlight> */}
-    </ScrollView>
+    
+        <Text style = {{marginTop: 15, fontSize:20, textAlign:'center'}}>Relevant Links</Text>
+        
+          {props.linkMap.map((links) => (
+            <TouchableHighlight onPress={() => onLinkPress(links.url)}>
+            <View>
+              <Text style ={{textDecorationLine: 'underline', color: 'blue', marginBottom: 10, marginTop: 10, textAlign:'center', paddingBottom:20}}>{links.title}</Text>
+            </View>
+          </TouchableHighlight> 
+          ))}
+        
+        
+        {/* <TouchableHighlight onPress={onLinkPress}>
+          <View>
+            <Text style ={{textDecorationLine: 'underline', color: 'blue', marginBottom: 10, marginTop: 10}}>TechOnTheNet</Text>
+          </View>
+        </TouchableHighlight> */}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
