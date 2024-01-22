70abb7a4fcf663f4d73c0b1ee76bb3c588af46d0
Evan-Stegall <evanstegall007@gmail.com>
2024-01-21 21:13:00 -0600

    add some more CSS to activity component to match figma

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -6,25 +6,20 @@ import { useState } from 'react';
 
 
 export default function Dashboard(){
-    const [dashInfo, setDashInfo] = useState({Steps: 130,Distance: "10 MI",Time: "40 MIN"});
+    const [dashInfo, setDashInfo] = useState({Steps: 10,Distance: "10km",Time: "3:00"});
     return(
-        <View>
-            <View>
-                <Text style={styles.today}>Today</Text>
+        <View style={styles.container}>
+            <View style={styles.subcontainer}>
+                <Text style={styles.text_activity}>Steps</Text>
+                <Text style={styles.userInfo}>{dashInfo.Steps}</Text>
             </View>
-            <View style={styles.container}>
-                <View style={styles.subcontainer}>
-                    <Text style={styles.text_activity}>Steps</Text>
-                    <Text style={styles.userInfo}>{dashInfo.Steps}</Text>
-                </View>
-                <View style={styles.subcontainer}>
-                    <Text style={styles.text_activity}>Distance</Text>
-                    <Text style={styles.userInfo}>{dashInfo.Distance}</Text>
-                </View>
-                <View style={styles.subcontainer}>
-                    <Text style={styles.text_activity}>Time</Text>
-                    <Text style={styles.userInfo}>{dashInfo.Time}</Text>
-                </View>
+            <View style={styles.subcontainer}>
+                <Text style={styles.text_activity}>Distance</Text>
+                <Text style={styles.userInfo}>{dashInfo.Distance}</Text>
+            </View>
+            <View style={styles.subcontainer}>
+                <Text style={styles.text_activity}>Time</Text>
+                <Text style={styles.userInfo}>{dashInfo.Time}</Text>
             </View>
         </View>
 
@@ -35,8 +30,7 @@ const styles = StyleSheet.create({
     container: {
         flexShrink: 0,
         flexDirection: "row",
-        backgroundColor: "#103158",
-        
+        backgroundColor: "#3C7FCB",
         borderRadius:10,
         justifyContent: "space-between",
         paddingHorizontal: 10,
@@ -48,26 +42,15 @@ const styles = StyleSheet.create({
         //flexDirection: "column",
         padding:15,
         justifyContent:"space-between",
-        fontSize:20,
-        
+        fontSize:20
     },
     text:{
         marginBottom:10,
-        
-    },
-
-    today:{
-        marginTop: 12,
-        marginLeft:10,
-        fontSize: 20,
-        fontWeight: "500"
-
-        
+        color: "white"
     },
     text_activity: {
         fontWeight: "300",
         fontSize: 15,
-        marginBottom: 8,
         color: "white"
     },
 
