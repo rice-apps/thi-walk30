9fc3246c78e5d989da42c5be1835e49ed7fdfcc0
shannonshao <90943847+shannonshao@users.noreply.github.com>
2024-01-27 17:00:55 -0600

    refactor code and small style fixes

--- app/pages/EventPage.tsx
+++ app/pages/EventPage.tsx
@@ -23,6 +23,19 @@ export function EventPage(props: { event: EventData }) {
 
   const styles = StyleSheet.create({
     header: {
+      color: "#18345c",
+      marginBottom: 10,
+      marginTop: 10,
+      fontSize: 18,
+      fontWeight: '600'
+    },
+    iconTextPair: {
+      flexDirection: 'row',
+      marginBottom: 5,
+    },
+    iconText: {
+      marginLeft: 10,
+      fontSize: 15,
       color: "#18345c"
     }
   });
@@ -38,44 +51,38 @@ export function EventPage(props: { event: EventData }) {
 
       <View style={{ margin: 20 }}>
         <Text style={{ fontSize: 30, fontWeight: '600', color: "#18345c", marginBottom: 10 }}>{props.event.title}</Text> 
-        {/* BOLD host name */}
-        <Text style={{ fontSize: 15, color: "#407ccc", marginBottom: 10 }}>Hosted by {props.event.host}</Text>
+        {/* TODO: BOLD host name */}
+        <Text style={{ fontSize: 15, color: "#407ccc", marginBottom: 15 }}>Hosted by {props.event.host}</Text>
         
-        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
           <View style={{ flexDirection: 'column' }}>
-            <View style={{ flexDirection: 'row', marginBottom: 5 }}>
+            <View style={styles.iconTextPair}>
               <Icon name="calendar" size={20} color="#18345c"></Icon>
-              <Text style={{ fontSize: 15, marginLeft: 10 }}>{props.event.date.toLocaleDateString(undefined, {month: "numeric", day: "numeric"})}</Text>
+              <Text style={styles.iconText}>{props.event.date.toLocaleDateString(undefined, {month: "short", day: "numeric", year: "numeric"})}</Text>
             </View>
-            <View style={{ flexDirection: 'row', marginBottom: 5  }}>
-              <LocationPin name="location-pin" size={20} color="#18345c"></LocationPin>
-              <Text style={{ fontSize: 15, marginLeft: 10 }}>{props.event.location.address}</Text>
-            </View>
-            <View style={{ flexDirection: 'row', marginBottom: 5  }}>
-              <Icon name="link" size={20} color="#407ccc"></Icon>
-              <Text style={{ fontSize: 15, marginLeft: 10, color: "#407ccc" }}>{props.event.link}</Text>
-            </View>
-          </View>
-          <View>
-            <Button onPress={handleButtonClick} title={registerText} color="#10446c"/>
-          </View>
-        </View>
-        
-        
-        
-        
 
-        {/* <Text style={styles.header}>23 participating</Text>
+            <View style={styles.iconTextPair}>
+              <LocationPin name="location-pin" size={20} color="#18345c"></LocationPin>
+              <Text style={styles.iconText}>{props.event.location.address}</Text>
+            </View>
+
+            {/* TODO: cut off link if too long */}
+            <View style={styles.iconTextPair}>
+              <Icon name="link" size={20} color="#407ccc"></Icon>
+              <TouchableHighlight onPress={() => onLinkPress(props.event.link)}>
+                <Text style={{ fontSize: 15, marginLeft: 10, color: "#407ccc"}}>{props.event.link}</Text>
+              </TouchableHighlight>   
+            </View>
+          </View>
+          {/* TODO: style button */}
+          <Button onPress={handleButtonClick} title={registerText} color="#10446c"/>
+        </View>
+
+        <Text style={styles.header}>23 participating</Text>
         <Text style={styles.header}>Challenges</Text>
         <Text style={styles.header}>Event Description</Text>
-        <Text>{props.event.description}</Text> */}
+        <Text>{props.event.description}</Text>
       </View>
-        
-      {/* <TouchableHighlight onPress={() => onLinkPress(props.event.link)}>
-        <View>
-          <Text style ={{textDecorationLine: 'underline', color: 'blue', marginBottom: 10, marginTop: 10}}>Link title</Text>
-        </View>
-      </TouchableHighlight>  */}
     </ScrollView>
   );
 };
