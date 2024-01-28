2c0addbd9e11cd8b3acb34860aee2e156bd720f4
shannonshao <90943847+shannonshao@users.noreply.github.com>
2024-01-27 20:52:16 -0600

    Style register button, format with prettier

--- app/pages/EventPage.tsx
+++ app/pages/EventPage.tsx
@@ -1,81 +1,146 @@
-import React, { useState } from 'react';
-import { ScrollView, View, Text, Button, TouchableHighlight, Linking, Image, StyleSheet } from 'react-native';
-import { EventData } from '../types/EventData';
+import React, { useState } from "react";
+import {
+  Linking,
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
+import { EventData } from "../types/EventData";
 import Icon from "@expo/vector-icons/Entypo";
 import LocationPin from "@expo/vector-icons/MaterialIcons";
 
 export function EventPage(props: { event: EventData }) {
-  const [registerText, setRegisterText] = useState('Register');
-  const [showRegister, setShowRegister] = useState(false)
-  
+  const [registerText, setRegisterText] = useState("Register");
+  const [showRegister, setShowRegister] = useState(false);
+
   const handleButtonClick = () => {
     setShowRegister(!showRegister);
     if (showRegister) {
-      setRegisterText('Register');
+      setRegisterText("Register");
     } else {
-      setRegisterText('Unregister')
+      setRegisterText("Unregister");
     }
   };
 
-  const onLinkPress = (link:string) => {
+  const onLinkPress = (link: string) => {
     Linking.openURL(link);
   };
 
   const styles = StyleSheet.create({
     header: {
-      color: "#18345c",
+      color: "#00426e",
       marginBottom: 10,
       marginTop: 10,
       fontSize: 18,
-      fontWeight: '600'
+      fontWeight: "600",
     },
     iconTextPair: {
-      flexDirection: 'row',
+      flexDirection: "row",
       marginBottom: 5,
     },
     iconText: {
       marginLeft: 10,
       fontSize: 15,
-      color: "#18345c"
-    }
+      color: "#00426e",
+    },
+    registerButton: {
+      backgroundColor: "#00426e",
+      borderRadius: 5,
+      padding: 10,
+      height: 45,
+      width: 120,
+      position: "absolute",
+      right: 0,
+      bottom: 0,
+    },
   });
 
   return (
-    <ScrollView style={{ backgroundColor: "#f8f4fc" }}> 
+    <ScrollView style={{ backgroundColor: "#f8f4fc" }}>
+      {/* Event image */}
       <Image
-        style = {{ width: '100%', height: 250}}
+        style={{ width: "100%", height: 250 }}
         source={{
           uri: props.event.featureImage,
         }}
       />
-
+      {/* Title and host */}
       <View style={{ margin: 20 }}>
-        <Text style={{ fontSize: 30, fontWeight: '600', color: "#18345c", marginBottom: 10 }}>{props.event.title}</Text> 
-        {/* TODO: BOLD host name */}
-        <Text style={{ fontSize: 15, color: "#407ccc", marginBottom: 15 }}>Hosted by {props.event.host}</Text>
-        
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <View style={{ flexDirection: 'column' }}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "600",
+            color: "#00426e",
+            marginBottom: 10,
+          }}
+        >
+          {props.event.title}
+        </Text>
+        <Text style={{ fontSize: 15, color: "#407ccc", marginBottom: 15 }}>
+          Hosted by{" "}
+          <Text style={{ fontWeight: "700" }}>{props.event.host}</Text>
+        </Text>
+
+        {/* Logistics box: date, location, link, register button */}
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <View style={{ flexDirection: "column" }}>
             <View style={styles.iconTextPair}>
-              <Icon name="calendar" size={20} color="#18345c"></Icon>
-              <Text style={styles.iconText}>{props.event.date.toLocaleDateString(undefined, {month: "short", day: "numeric", year: "numeric"})}</Text>
+              <Icon name="calendar" size={20} color="#00426e"></Icon>
+              <Text style={styles.iconText}>
+                {props.event.date.toLocaleDateString(undefined, {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </Text>
             </View>
 
             <View style={styles.iconTextPair}>
-              <LocationPin name="location-pin" size={20} color="#18345c"></LocationPin>
-              <Text style={styles.iconText}>{props.event.location.address}</Text>
+              <LocationPin
+                name="location-pin"
+                size={20}
+                color="#00426e"
+              ></LocationPin>
+              <Text style={styles.iconText}>
+                {props.event.location.address}
+              </Text>
             </View>
 
             {/* TODO: cut off link if too long */}
             <View style={styles.iconTextPair}>
               <Icon name="link" size={20} color="#407ccc"></Icon>
               <TouchableHighlight onPress={() => onLinkPress(props.event.link)}>
-                <Text style={{ fontSize: 15, marginLeft: 10, color: "#407ccc"}}>{props.event.link}</Text>
-              </TouchableHighlight>   
+                <Text
+                  style={{
+                    fontSize: 15,
+                    marginLeft: 10,
+                    color: "#407ccc",
+                    overflow: "hidden",
+                  }}
+                >
+                  {props.event.link}
+                </Text>
+              </TouchableHighlight>
             </View>
           </View>
-          {/* TODO: style button */}
-          <Button onPress={handleButtonClick} title={registerText} color="#10446c"/>
+          <View style={styles.registerButton}>
+            <Pressable onPress={handleButtonClick}>
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 18,
+                  fontWeight: "700",
+                  textAlign: "center",
+                }}
+              >
+                {registerText}
+              </Text>
+            </Pressable>
+          </View>
         </View>
 
         <Text style={styles.header}>23 participating</Text>
@@ -85,7 +150,4 @@ export function EventPage(props: { event: EventData }) {
       </View>
     </ScrollView>
   );
-};
-
-
-
+}
