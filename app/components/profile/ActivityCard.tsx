8b23e9cb3dcf03bb03528dc9f234404e100d3b56
Evan-Stegall <evanstegall007@gmail.com>
2024-02-12 17:56:27 -0600

    Added some documentation and style

--- app/components/profile/ActivityCard.tsx
+++ app/components/profile/ActivityCard.tsx
@@ -9,22 +9,98 @@ type Route = {
     key: string;
     title: string;
 };
-  
+
 type State = NavigationState<Route>;
 
+
+type DateRange = {
+    start_date: Date, 
+    end_date: Date
+}
+
+/*
+ Hardcoded Dummy Data, will be replaced by API calls
+*/
 const daily_avg = 130;
 const Total = 1400;
 const steps_dta =[ {value:380, label: "Mon"}, {value:150, label: "Tue"}, {value:300, label: "Wed"}, {value:50, label: "Thu"}, {value:360, label: "Fri"}, {value:370, label: "Sat"}, {value:230, label: "Sun"} ]
+const other_steps_dta =[ {value:380, label: "Mon"}, {value:150, label: "Tue"}, {value:300, label: "Wed"}, {value:50, label: "Thu"}, {value:360, label: "Fri"}, {value:370, label: "Sat"}, {value:230, label: "Sun"} ].reverse()
 const weeks_data =[ {value:3000, label: "Week 1"}, {value:2500, label: "Week 2"}, {value:2750, label: "Week 3"}, {value:1000, label: "Week 4"} ]
-//Hard coded values for now...will replace with fetched data later
-let activity_to_data = new Map<string, barDataItem[][]>(
-    [
-        ["steps", [steps_dta, weeks_data]],
-        ["distance", [steps_dta, weeks_data]],
-        ["time", [steps_dta, weeks_data]],
-    ]
-);
 
+/**
+ * Returns the data for each of the three segments of data (Steps, Distance, Time) that we collect
+ */
+function getData(currentDateRange: DateRange): Map<string, barDataItem[][]> {
+    let activity_to_data = new Map<string, barDataItem[][]>(
+        [
+            ["steps", [steps_dta, weeks_data]],
+            ["distance", [steps_dta, weeks_data]],
+            ["time", [steps_dta, weeks_data]],
+        ]
+    );
+
+    // trying to introduce some random behavior
+    if (currentDateRange.start_date.getMilliseconds() % 2 == 0) {
+        activity_to_data = new Map<string, barDataItem[][]>(
+            [
+                ["steps", [other_steps_dta, weeks_data]],
+                ["distance", [other_steps_dta, weeks_data]],
+                ["time", [other_steps_dta, weeks_data]],
+            ]
+        )
+    }
+
+
+    return activity_to_data;
+}
+
+/**
+ * Initializes our DateRange as a one week interval ending on the current day
+ * @returns 
+ */
+function initDateRange(): DateRange {
+    let start_date: Date = new Date()
+    let end_date = new Date();
+    start_date.setDate(end_date.getDate() - 7);
+    return {
+        start_date,
+        end_date
+    }
+}
+
+/**
+ * Callback function that gets called whenever a user queries a different date range
+ * @param currentDate - currentDateRange object being used on the screen
+ * @param back true if the user wants to go back one week, false otherwise
+ * @returns 
+ */
+function updateDateRange(currentDate: DateRange, back: boolean): DateRange {
+    let start_date = new Date();
+    let end_date = new Date();
+    let one_week_in_milli_sec = back ? (-1.0 * 604800000) : (604800000);
+    start_date.setTime(currentDate.start_date.getTime() + one_week_in_milli_sec);
+    end_date.setTime(currentDate.end_date.getTime() + one_week_in_milli_sec);
+
+    return {
+        start_date,
+        end_date
+    }
+}
+
+/**
+ * Extracts the date and month from a Date String (i.e. 12/31/2024 -> 12/31)
+ * @param date_str - the date string to extract from
+ * @returns 
+ */
+function extractDayAndMonth(date_str: Date) {
+    return date_str.toLocaleDateString().substring(0, date_str.toLocaleDateString().lastIndexOf("/"))
+}
+
+/**
+ * Creates components for each of the tabs 
+ * @param type - can either be steps, distance, or time. This is used to indicate which data to use when generating the charts
+ * @returns The component that is displayed within one of the tabs
+ */
 function makeRoute(type: string) {
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState("Weekly");
@@ -32,12 +108,22 @@ function makeRoute(type: string) {
       {label: 'Weekly', value: 'Weekly'},
       {label: 'Monthly', value: 'Monthly'}
     ]);
+
+    let current_date_range: DateRange = initDateRange();
+    let activity_to_data = getData(current_date_range);
+    if (activity_to_data === undefined) {
+        return <View>No Data</View>
+    }
     let data_list: barDataItem[][] = activity_to_data.get(type)!;
-    let data: barDataItem[] = [];
     if (data_list === undefined) {
         return (<View>Could not Load Data!</View>);
     }
+    let data: barDataItem[] = [];
+    const [dates, setDates] = useState(current_date_range);
+
     let bar_width: number = 0;
+
+    //TODO abstractify all of the setup into one function 
     switch (value) {
         case 'Weekly':
             data = data_list[0];
@@ -48,10 +134,11 @@ function makeRoute(type: string) {
             bar_width = 50;
             break;
     }
+
     return (
         <View>
             {/* First Row with BarChart */}
-            <View style={{ flexDirection: 'row', alignItems: "flex-start", justifyContent: "center", alignContent: "center", marginBottom: 20, paddingTop: 10, zIndex: 9999}}>
+            <View style={{ flexDirection: 'row', alignItems: "flex-start", justifyContent: "center", alignContent: "center", margin: 20, paddingTop: 10, zIndex: 9999}}>
                 <DropDownPicker
                         style={styles.dropDown}
                         open={open}
@@ -67,6 +154,7 @@ function makeRoute(type: string) {
                             justifyContent: "center",
                             zIndex: 9999,
                         }}
+                        
                     />
                     <Text style = {{marginRight: 30, marginLeft: 30}}>
                         <Text variant="bodySmall">Daily Avg. </Text>
@@ -77,7 +165,24 @@ function makeRoute(type: string) {
                         <Text style = {{fontWeight: "bold"}}variant="bodyLarge">{Total}</Text>
                     </Text>
             </View>
-            {/* Second Row with BarChart */}
+            {/* Second Row with interactive dates */}
+            <View style = {{margin: 20}}>
+                <View style = {{justifyContent: "center", alignItems: "center", flexDirection: "row"}}>
+                    <Text style = {{fontWeight: "bold", fontSize: 25, marginRight: 15}} onPress={() => {
+                        let new_range = updateDateRange(dates, true);
+                        setDates(new_range);
+                    }
+                    }>{"<"}</Text>
+                    <Text style = {{fontWeight: "bold", fontSize: 25}}>{extractDayAndMonth(dates.start_date)}</Text>
+                    <Text style = {{fontWeight: "bold", fontSize: 25}}>-</Text>
+                    <Text style = {{fontWeight: "bold", fontSize: 25}}>{extractDayAndMonth(dates.end_date)}</Text>
+                    <Text style = {{fontWeight: "bold", fontSize: 25, marginLeft: 15}} onPress={() => {
+                        let new_range = updateDateRange(dates, false);
+                        setDates(new_range);
+                    }}>{">"}</Text>
+                </View>
+            </View>
+            {/* Third Row with BarChart */}
             <View style = {{marginTop: 20}}>
                 <BarChart 
                     data = {data} 
@@ -91,11 +196,16 @@ function makeRoute(type: string) {
                     xAxisColor={'white'}
                     yAxisColor={'white'}
                     />
-                </View>
+            </View>
         </View>
     )
 };
 
+/**
+ * Functions to create each of the Tabs. The TabView documentation indicated that using arrow functions results in 
+ * undefined behavior, so this is the workaround. 
+ * @returns 
+ */
 function StepsRoute() {
     return makeRoute('steps');
 };
@@ -150,7 +260,7 @@ export default function Dashboard() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        height: 330,
+        height: 425,
         margin: 10,
     },
     TabBar: {
@@ -175,7 +285,8 @@ const styles = StyleSheet.create({
         height:0,
         paddingHorizontal: 10,
         minHeight: 30,
-        zIndex: 1000
+        zIndex: 1000,
+        borderWidth: 0
     },
     selectRow: {
 
