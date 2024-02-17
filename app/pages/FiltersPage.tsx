077542aeed90ff5192ecdffd2cae01b2c9ce6e4d
jonathanlee <jonathanwxlee@gmail.com>
2024-02-17 15:45:40 -0600

    more work lol

--- app/pages/FiltersPage.tsx
+++ app/pages/FiltersPage.tsx
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 import {ScrollView, Text, View, StyleSheet, Switch} from "react-native";
 
 export function FiltersPage() {
+        // filters box below
         const [myEvent, setMyEvent] = useState(false);
         const [openReg, setOpenReg] = useState(false);
+
+        // sort by box below
+        const [dateSort, setDateSort] = useState(false);
+        const [distanceSort, setDistanceSort] = useState(false);
+        const [participantsSort, setParticipantsSort] = useState(false);
+
+        // date filter box below
+        const [fromDate, setFromDate] = useState(null);
+        const [toDate, setToDate] = useState(null);
+        
+
         const styles = StyleSheet.create({
                 container: {
                         flexDirection: "column",
@@ -12,6 +24,9 @@ export function FiltersPage() {
                 filterText: {
                         fontSize: 20,
                         marginBottom: 20,       
+                },
+                switchbox: {
+                        flexDirection: "row"
                 }
                 
 
@@ -19,19 +34,59 @@ export function FiltersPage() {
 
 
         const Filterbox = () => {
+                const togglemyEvent = () => setMyEvent(previousState => !previousState);
+                const toggleopenReg = () => setOpenReg(previousState => !previousState);
                 return (
                         <View style = {styles.container}>
                            <Text style = {styles.filterText}>Filter</Text>
-                           <Switch></Switch>    
+                           
+                           <View style = {styles.switchbox}>
+                                <Text>Only My Events</Text>
+                                <Switch
+                                onValueChange={togglemyEvent}
+                                value = {myEvent}/>
+                           </View> 
+                           <View style = {styles.switchbox}>
+                                <Text>Registration Open</Text>
+                                <Switch
+                                onValueChange={toggleopenReg}
+                                value = {openReg}
+                                />
+
+                           </View>  
                         </View>
                 )
                 
         }
 
         const SortByBox = () => {
-          const [dateSort, setDateSort] = useState(false);
-          const [distanceSort, setDistanceSort] = useState(false);
-          const [participantsSort, setParticipantsSort] = useState(false);
+          const styles = StyleSheet.create({
+            title: {
+              fontSize: 21,
+            },
+
+            parentBox: {
+              display: "flex",
+              width: "100%",
+              flexDirection: "column"
+            },
+
+            sortOptions: {
+              display: "flex",
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }
+          })
+
+          return (
+            <View style = {styles.parentBox}>
+              
+            </View>
+          )
+        }
+
+        const DateBox = () => {
           const styles = StyleSheet.create({
             title: {
               fontSize: 21,
@@ -54,7 +109,7 @@ export function FiltersPage() {
         return (
                 <ScrollView>
                         <Text>Open up App.tsx to start working on your app!</Text>
-
+                        <Filterbox/>
                 </ScrollView>
         )
 
