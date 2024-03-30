7db5b3785f6a84df15ace7d0723c710bd4c72825
TheSacredJump <padhu.sathya@gmail.com>
2024-03-30 15:57:06 -0500

    Stat Routes

---
+++ server/routes/stat.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const Stat = require('../models/stat');
+
+router.post('/create', async (req, res) => {
+    console.log("Attempting to create a new stat...")
+    try {
+      const { type, ...rest } = req.body;
+      const newStat = new Stat({
+        type,
+        data: rest, // rest contains all other fields besides type
+      });
+
+      newStat.markModified('data');
+  
+      const savedStat = await newStat.save();
+      res.status(201).json(savedStat);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
+
+// Get a specific stat by ID
+router.get('/:id', async (req, res) => {
+    try {
+        const stat = await Stat.findById(req.params.id);
+    if (!stat) {
+      return res.status(404).json({ message: 'Stat not found' });
+    }
+    res.json(stat);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//Get stats by user ID
+router.get('/:userID/stats', async (req, res) => {
+    try {
+        const stats = await Stat.find({ userID: req.params.userID });
+        res.json(stats);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//Get stats by event ID
+router.get('/:eventID/stats', async (req, res) => {
+    try {
+        const stats = await Stat.find({ eventID: req.params.eventID });
+        res.json(stats);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const stat = await Stat.findById(req.params.id);
+        if (!stat) {
+            return res.status(404).json({ message: 'Stat not found' });
+        }
+        await stat.remove();
+        res.json({ message: 'Stat deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+  
+
+module.exports = router;
