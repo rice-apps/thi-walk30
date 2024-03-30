daa5a6a1c157bc0406c9dd48b65183c8b2efecfc
nolantuite <nrt5@rice.edu>
2024-03-30 14:26:58 -0500

    reverted to previous main

--- app/App.tsx
+++ app/App.tsx
@@ -1,27 +1,92 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import React from 'react'
-import { AnnouncementPage } from "./pages/AnnouncementPage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { constants } from "./Style";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ResourcesPage } from "./pages/ResourcesPage";
+import EventsPage from "./pages/EventsPage";
 import { EventPage } from "./pages/EventPage";
-import { EventList } from "./pages/EventList";
-import { EventCalendar } from "./pages/EventCalendar";
+import { Button } from "react-native-paper";
+import { EventData } from "./types/EventData";
+import ProfilePage from "./pages/ProfilePage";
 
-export default function App() {
-//   const exampleLink: LinkData = {
-//     title: "More Information",
-//     url: "https://www.example.com",
-// };
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function Home() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      {/* <AnnouncementPage title = {"Pumpkin-Carving and Costume Contests Ensure a Festive Halloween at THI"}
-      body= {"The team from Administration and Development — including Henry Aceves, Karen Gunay, Tanya Rojas, Daphne Singleterry, Marcia Strauss, Julie Voss, and Sheila Vrana — ultimately took home first place for their “Deep in the Heart of Texas” operating room vignette, complete with surgeon and patient."} 
-      linkMap={[exampleLink]}/>
-      <StatusBar style="auto" /> */}
-      {/* <EventPage title = {"Title"} body = {"body text"} date = {"date"} hostorg = {"hostorg"} linkMap = {[exampleLink]}/> */}
-      {/* <EventList /> */}
-      <EventCalendar />
+    <View style={styles.center}>
+      <StatusBar style="auto" />
+      <Text>Not implemented yet!</Text>
     </View>
+  );
+}
+
+function Tabs() {
+  function TabIcon(name: any, color: string) {
+    return (
+      <MaterialCommunityIcons name={name} color={color} size={26} />
+    );
+  }
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarInactiveTintColor: constants.gray,
+        tabBarActiveTintColor: constants.darkBlue
+      }} >
+      <Tab.Screen
+        name="Home"
+        children={ProfilePage}
+        options={{
+          tabBarIcon: ({ color }) => TabIcon("home", color)
+        }} />
+      <Tab.Screen
+        name="Events"
+        children={(navigator) => <EventsPage navigator={navigator} />}
+        options={{
+          tabBarIcon: ({ color }) => TabIcon("calendar", color)
+        }} />
+      <Tab.Screen
+        name="Resources"
+        component={ResourcesPage}
+        options={{
+          tabBarIcon: ({ color }) => TabIcon("information", color)
+        }} />
+      <Tab.Screen
+        name="Updates"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => TabIcon("bell", color)
+        }} />
+    </Tab.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <Tabs />
+  );
+}
+
+export default function AppStack() {
+  return (
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Tabs" component={App} options={{
+            headerShown: false
+          }} />
+          <Stack.Screen name="Event" children={(navigator) => <EventPage navigator={navigator} />} />
+        </Stack.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
 
@@ -29,7 +94,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "stretch",
-    justifyContent: "center",
+  },
+  center: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
