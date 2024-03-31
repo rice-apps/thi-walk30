2e9f04ffbcbd459d59614128fb40ecc80f14038c
Evan-Stegall <evanstegall007@gmail.com>
2024-03-31 17:46:02 -0500

    Basic components for Organization Registration.

---
+++ app/components/OrganizationRegistration.tsx
@@ -0,0 +1,129 @@
+import React, {useState, useEffect} from 'react';
+import {View, ScrollView, Text, Image, Pressable, TextInput} from 'react-native';
+import { StyleSheet } from 'react-native';
+import Icon from '@expo/vector-icons/FontAwesome';
+import UploadOrganizationButton from './UploadOrganizationButton';
+
+const uploadImage = () => {
+    console.log("Pressed Upload Image button");
+}
+
+const registerOrganization = () => {
+    console.log("Pressed Register Organization button");
+}
+
+function OrganizationRegistration() {
+    const [org_name, setOrgName] = useState("");
+    const [org_phone, setOrgPhone] = useState("");
+    const [org_email, setOrgEmail] = useState("");
+
+    return (
+        <ScrollView>
+            <View style = {styles.container}>
+                {/* Title*/}
+                <View style = {{marginTop: 20}}>
+                    <Text style = {styles.title} >Register your organization</Text>
+                </View>
+                {/*Form to upload image*/}
+                    <UploadOrganizationButton/>
+                {/* Forms for organization metadata */}
+                <View>
+                    {/* Organization Name */}
+                    <View style={styles.block}>
+                        <Text style={styles.titleText}>Organization Name</Text>
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: "space-between", borderWidth: 1, borderRadius: 5}}>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={(value) => setOrgName(value)}
+                                    placeholder="Name"
+                                    placeholderTextColor="#00426e"
+                                    autoCapitalize="none"
+                                />
+                            <Icon name="user" size={24} color="black" style = {{marginRight: 10, marginTop: 5}}/>
+                        </View>
+                    </View>
+                    {/* Phone Number */}
+                    <View style={styles.block}>
+                        <Text style={styles.titleText}>Phone Number</Text>
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: "space-between", borderWidth: 1, borderRadius: 5}}>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={(value) => setOrgPhone(value)}
+                                    placeholder="Phone Number"
+                                    placeholderTextColor="#00426e"
+                                    autoCapitalize="none"
+                                />
+                            <Icon name="phone" size={24} color="black" style = {{marginRight: 10, marginTop: 5}}/>
+                        </View>
+                    </View>
+                    <View style={styles.block}>
+                        <Text style={styles.titleText}>Email</Text>
+                        <View style = {{flex: 1, flexDirection: 'row', justifyContent: "space-between", borderWidth: 1, borderRadius: 5}}>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={(value) => setOrgEmail(value)}
+                                    placeholder="Phone Number"
+                                    placeholderTextColor="#00426e"
+                                    autoCapitalize="none"
+                                />
+                            <Icon name="envelope" size={24} color="black" style = {{marginRight: 10, marginTop: 5}}/>
+                        </View>
+                    </View>
+                </View>
+                {/* Register button */}
+                <View style = {{flex:1, justifyContent: 'center', alignItems: "center"}}>
+                    <Pressable style={styles.submit} onPress = {registerOrganization}>
+                            <Text style = {styles.text}>Submit</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        margin: 25
+    },
+    title: {
+        //Write css to center the text in the middle o the screen
+        textAlign: 'center',
+        color: "#00426D",
+        fontSize: 24,
+        fontWeight: "600",
+    },
+    block: {
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    input: {
+        padding: 10,
+        borderRadius: 5,
+    },
+    titleText: {
+        color: "#00426e",
+        marginBottom: 10,
+        marginTop: 10,
+        fontSize: 17,
+        fontWeight: "500",
+      },
+    submit: {
+        width: 138.38,
+        height: 44,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#006983",
+    },
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+      },
+});
+
+export default OrganizationRegistration;
---
+++ app/components/UploadOrganizationButton.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import { Image, View, Platform, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+import { Pressable } from 'react-native';
+import Icon from '@expo/vector-icons/FontAwesome';
+
+export default function UploadOrganizationButton() {
+  const [image, setImage] = useState('');
+  const addImage = async () => {
+    let _image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [16,9],
+      quality: 1,
+    });
+    if (_image.canceled === false) {
+        setImage(_image.assets[0]['uri']);
+    }
+  };
+
+  const clearImage = () => {
+    setImage('');
+  }
+
+  const uploadImage = async () => {
+    console.log("Pressed Upload Image button");
+  }
+  
+  return (
+        <View>
+            <TouchableOpacity onPress={addImage}>
+                <View style={imageUploaderStyles.container}>
+                    {
+                    image  && <Image source={{ uri: image }} style={{ width: 300, height: 200 }} />
+                    } 
+                    {
+                    !image && <AntDesign name="camerao" size={image ? 20 : 50} color="blue" />
+                    }
+                    <Text>{image ? 'Change' : 'Upload An'} Event Image</Text>
+                </View>
+            </TouchableOpacity>
+            <View style = {imageUploaderStyles.button_row}>
+              <Pressable onPress={uploadImage} style = {imageUploaderStyles.button}>
+                      <Text style = {imageUploaderStyles.text}>Upload Image</Text>
+              </Pressable>
+              <Icon name="trash" size={24} color="black" onPress={clearImage} style = {{marginLeft: 10}}/>
+            </View>
+        </View>
+  );
+}
+const imageUploaderStyles=StyleSheet.create({
+    container:{
+        elevation:2,
+        height:220,
+        width:300,
+        backgroundColor:'#efefef',
+        position:'relative',
+        borderRadius:15,
+        overflow:'hidden',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '10%',
+        marginBottom: 'auto',
+        display:'flex',
+        alignItems:"center",
+        justifyContent:'center',
+        verticalAlign: 'middle'
+    },
+    button: {
+      width: 153.01,
+      height: 39.49,
+      borderRadius: 5,
+      backgroundColor: "#006983",
+    },
+    button_row: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      alignItems: 'center',
+      flexDirection: 'row',
+      marginTop: 10
+    },
+})
